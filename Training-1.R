cbc4e9647b47a40d97211988209637c0675c07d4
Calif-Owaka <califowaka@gmail.com>
2016-06-23 08:49:29 +0300

    Update Training-1.R

--- Training-1.R
+++ Training-1.R
@@ -1,11 +1,19 @@
 data
+#retrieve the dimension of the object data
 dim(data)
+#assign subject to an object data[1]
 subject <- data[1]
+#assign dates to an object data[4]
 dates <- data[4]
+#bind columnwise the objects subject and dates
 cbind(subject, dates)
+#assign to cl1 the first row of the object data
 cl1 <- data[1,]
+#assign to cl18 the 150th row of the object data
 cl18 <- data[150,]
+#bind the objects cl1 and cl18 and assign the combination to rowbind
 rowbind<-rbind(cl1, cl18)
+#find respective means of each column
 colMeans(data[1])
 mean(data$subject)
 attach(data)
@@ -15,8 +23,10 @@ attach(dates)
 c<-c(1,"e",32,32,3)
 attach(c)
 sx<-attributes(data)
+#to determine the class of the object sx
 class(sx)
 sx$class
+#change the class of the object sl to list
 sl<-as.list(sx)
 class(sl)
 names(data)
@@ -30,12 +40,14 @@ sum(subject)
 complete.cases(removeq10)
 complete.cases(data)
 
-
+#create a sequence with range from 1 to 10 with 10 elements 
 x<- seq(0,1, length=10)
 x
+#to get the length of the object data
 length(data)
 length(data$intakeq1)
 seq_along(x)
+#forming a sequence of element "timon" that is repeated 20 times
 rep("timon", 20)
 subject[[1]]
 data$subject[is.na(data$datecomp)]
